48d5f029e226ae38512d6c6149eb55611be43e6c
progfay <shun1856shun@gmail.com>
2019-12-01 17:30:23 +0900

    compress files with level 9 (55 -> 92)

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const express = require('express');
+const compression = require('compression');
 
 const app = express();
 
 app.use(express.static('.'));
+app.use(compression({ level: 9 }));
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Please access to http://localhost:3000/2019/speakers')
