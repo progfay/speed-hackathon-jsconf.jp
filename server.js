5121b8aa7bd8e31ed638bcd4df0cfef346708f69
progfay <shun1856shun@gmail.com>
2019-12-01 17:45:48 +0900

    use webp format (92 -> 96)

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const compression = require('compression');
 
 const app = express();
 
-app.use(express.static('.'));
 app.use(compression({ level: 9 }));
+app.use(express.static('.'));
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Please access to http://localhost:3000/2019/speakers')
